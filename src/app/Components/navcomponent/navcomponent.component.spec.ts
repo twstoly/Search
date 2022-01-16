415541fa1ebcb7bfe63be9ad2f76d37745c4ab3b
Oliver <oliver.mwangale@moringaschool.com>
2022-01-16 15:16:47 +0300

    add navigation component that adds a nav bar t our application

---
+++ src/app/Components/navcomponent/navcomponent.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NavcomponentComponent } from './navcomponent.component';
+
+describe('NavcomponentComponent', () => {
+  let component: NavcomponentComponent;
+  let fixture: ComponentFixture<NavcomponentComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NavcomponentComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NavcomponentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
