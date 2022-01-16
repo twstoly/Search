74f5a7b861c8298b9f2f9f45a9a9326da11182e2
Oliver <oliver.mwangale@moringaschool.com>
2022-01-16 16:03:00 +0300

    iupdae the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { ServiceRequestService } from './Services/service-request.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Search';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BackgroundDirective } from './Directives/background/background.directive';
+import { CustomPipePipe } from './Pipes/customPipe/custom-pipe.pipe';
+import { NavcomponentComponent } from './Components/navcomponent/navcomponent.component';
+import { FooterComponent } from './Components/footer/footer/footer.component';
+import { MainControlComponent } from './Components/main-control/main-control.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackgroundDirective,
+    CustomPipePipe,
+    NavcomponentComponent,
+    FooterComponent,
+    MainControlComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
